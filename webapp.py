d402e63862aedd5a5f1769ebf5d5021f21872282
aliciah18-meet <ahandal@js-bethlehem.com>
2017-02-05 18:55:14 +0200

    kkk

--- webapp.py
+++ webapp.py
@@ -1,1 +1,16 @@
-from flask import flask
+from flask import Flask
+from model import *
+
+app = Flask(__name__)
+
+engine = create_engine('sqlite:///fizzbuzz.db')
+Base.metadata.bind = engine
+DBsession = sessionmaker(bind=engine, qutoflush=False)
+session = DBsession()
+
+@app.route('/')
+def hello_world():
+	return 'Hello World'
+
+if __name__== '__main__':
+	app.run()
