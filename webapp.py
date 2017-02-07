70d4198d1c5960524acdc571065f0261b756de5c
nisreen18-meet <nisreenjanah234@gmail.com>
2017-02-07 19:49:52 +0200

    ready for first deployment

--- webapp.py
+++ webapp.py
@@ -1,16 +1,22 @@
-from flask import Flask
-from model import *
+from flask import Flask , request , render_template
+from Database import *
 
 app = Flask(__name__)
 
 engine = create_engine('sqlite:///fizzbuzz.db')
 Base.metadata.bind = engine
-DBsession = sessionmaker(bind=engine, qutoflush=False)
+DBsession = sessionmaker(bind=engine, autoflush=False)
 session = DBsession()
 
 @app.route('/')
-def hello_world():
-	return 'Hello World'
+@app.route('/login', methods=['GET','POST'])
+def login():
+	if request.method == 'GET':
+		return render_template('login.html')
+
+
+
+
 
 if __name__== '__main__':
-	app.run()
+	app.run(debug=True)
